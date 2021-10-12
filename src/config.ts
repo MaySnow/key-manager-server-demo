a4b24a0db8c7828e88c7025c9ea6cecb9f2ff827
yuxuemei@aixuexi.com <yuxuemei@aixuexi.com>
2021-10-12 12:01:07 +0800

    fix: .env path

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import * as path from "path";
 
-dotenv.config({ path: path.join(__dirname, ".env") });
+dotenv.config({ path: path.join(__dirname, "./.env") });
 
 export interface Config {
     port: number;
